da9537fcfec6d99bb63c839d1a4fd9a4f6cf5c1c
weicao <caowei@vtech.com>
2016-12-27 18:49:37 +0800

    模块管理器

--- src/default_module_manager.h
+++ src/default_module_manager.h
@@ -3,5 +3,32 @@
 #include <vector>
 #include "interfaces/module_interface.h"
 #include "default_service_manager.h"
+#include "default_view_manager.h"
+#include "default_event_manager.h"
 
+class default_module_manager
+{
+private:
+    std::vector<common_information<module_interface*> > modules;
+    default_event_manager event_manager;
+    default_service_manager service_manager;
+    default_view_manager view_manager;
+    
+public:
+    static default_module_manager instance;
+    
+public:
+    service_manager_interface* get_service_manager() const;
+    view_manager_interface* get_view_manager() const;
+    event_manager_interface* get_event_manager() const;
+    std::vector<common_information<module_interface*> > get_modules() &;
+    
+    void clear();
+    module_interface* get_module(std::wstring& name);
+    void regist(module_interface* module);
+    
+private:
+    default_module_manager();
+    ~default_module_manager();
+};
 
